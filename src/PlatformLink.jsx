536f2234fa7f8c98fdcde398705f4cf150fff7e3
Niklas Granqvist <niklasgranqvist@windowslive.com>
2025-11-20 17:22:14 +0200

    fixed bug with platform link colors

--- src/PlatformLink.jsx
+++ src/PlatformLink.jsx
@@ -1,24 +1,28 @@
 const PlatformLink = ({ title, description, Url, iconUrl, color }) => {
 
     let glowColor = "";
+    let borderColor = "";
     switch (color) {
 
         case 'pink':
-            glowColor = '255,0,100,0.6';
+            borderColor = 'border-pink-500/60';
+            glowColor = 'shadow-[inset_0_0_30px_rgba(255,0,100,0.6),0_0_10px_rgba(0,0,0,1)]';
             break;
         case 'green':
-            glowColor = '0,255,100,0.6';
+            borderColor = 'border-green-500/60';
+            glowColor = 'shadow-[inset_0_0_30px_rgba(0,255,100,0.6),0_0_10px_rgba(0,0,0,1)]';
             break;
         case 'cyan':
-            glowColor = '0,100,255,0.6';
+            borderColor = 'border-blue-500/60';
+            glowColor = 'shadow-[inset_0_0_30px_rgba(0,100,255,0.6),0_0_10px_rgba(0,0,0,1)]';
             break;
     }
     return (
         <a href={Url}
             className={`text-white! w-100 h-50 bg-zinc-950 border-2 flex flex-col items-center justify-center
                         bg-[radial-gradient(circle,rgba(75,75,75,0.75),transparent_80%)]
-                    border-${color}-500/60 
-                        shadow-[inset_0_0_30px_rgba(${glowColor}),0_0_10px_rgba(0,0,0,1)]
+                        ${borderColor} 
+                        ${glowColor}
         `}>
             <div className="flex gap-2">
                 <img className="w-12 h-12" src={iconUrl}></img>
